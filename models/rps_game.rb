d76a0d65afa595ff40afa1259e5ed027c078e189
cassidytrinh <cassidytrinh@gmail.com>
2018-06-26 18:35:49 +0000

    automatically backed up by learn

--- models/rps_game.rb
+++ models/rps_game.rb
@@ -11,8 +11,10 @@ class RPSGame
 
   def valid_play?(play)
     if play == "rock" || play == "paper" || play == "scissors"
+      puts play+"true"
       return true
     else
+      puts play+"false"
       return false
     end
   end
