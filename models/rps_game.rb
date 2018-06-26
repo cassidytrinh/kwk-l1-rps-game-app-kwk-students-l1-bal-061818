8a0493a290d6f5b0391381198d9da14abaa784b5
cassidytrinh <cassidytrinh@gmail.com>
2018-06-26 18:31:33 +0000

    automatically backed up by learn

--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,20 +1,16 @@
 class RPSGame
   attr_accessor :play
 
+  def initialize(play)
+    @play = play
+  end
+
   def valid_play?(play)
     if play == "rock" || play == "paper" || play == "scissors"
       return true
     else
       raise PlayTypeError
       return false
-    end
-  end
-
-  def initialize(play)
-    if valid_play?(play) == true
-      @play = play
-    else
-      raise PlayTypeError
     end
   end
 
