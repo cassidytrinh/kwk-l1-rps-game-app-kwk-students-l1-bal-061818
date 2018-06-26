df6ba82ad55c5e50998ed6f0c5119e1a39c3f178
cassidytrinh <cassidytrinh@gmail.com>
2018-06-26 18:24:44 +0000

    automatically backed up by learn

--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,6 +1,8 @@
 class RPSGame
   attr_accessor :play
 
+  def initialize(play)
+    @play = play
   class PlayTypeError < StandardError
     # Don't be alarmed! This is a class set inside a class, but works just fine in Ruby
     # In this case, we're creating a custom error that should be raised if RPSGame.new receives an invalid move as an argument
